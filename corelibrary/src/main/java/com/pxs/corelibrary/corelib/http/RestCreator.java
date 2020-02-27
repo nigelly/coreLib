d3ad787826a795f933689584586ce20eca20b519
“nigelly” <745319870@qq.com>
2020-02-27 14:01:41 +0800

    '更新拦截器'

--- corelibrary/src/main/java/com/pxs/corelibrary/corelib/http/RestCreator.java
+++ corelibrary/src/main/java/com/pxs/corelibrary/corelib/http/RestCreator.java
@@ -3,6 +3,7 @@ package com.pxs.corelibrary.corelib.http;
 
 import com.pxs.corelibrary.corelib.ConfigKeys;
 import com.pxs.corelibrary.corelib.ProjectInit;
+import com.pxs.corelibrary.corelib.tool.Logger;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -15,13 +16,14 @@ import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 
-public  class RestCreator {
+public class RestCreator {
+    private static String simpleName = "";
 
-    private static  class RetrofitHolder {
+    private static class RetrofitHolder {
 
-        private static  String BASE_URL = ProjectInit.getConfiguration(ConfigKeys.API_HOST_SING);
+        private static String BASE_URL = ProjectInit.getConfiguration(ConfigKeys.API_HOST_SING);
 
-        private static  Retrofit RETROFIT_CLIENT = new Retrofit
+        private static Retrofit RETROFIT_CLIENT = new Retrofit
                 .Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(ScalarsConverterFactory.create())
@@ -39,36 +41,58 @@ public  class RestCreator {
     }
 
     //单独配置okhttp
-    private static  class OkHttpHolder {
+    private static class OkHttpHolder {
 
         private static final int TIME_OUT = 60;
 
-        private static OkHttpClient.Builder builder = null;
+        private static OkHttpClient client = null;
+
+        private static OkHttpClient clientInterceptor = null;
+
 
         private static OkHttpClient OK_HTTP_CLIENT() {
-            if (builder == null) {
-                return new OkHttpClient.Builder()
+            if (client == null) {
+                return client = new OkHttpClient.Builder()
                         .connectTimeout(TIME_OUT, TimeUnit.SECONDS)
                         .build();
             }
-            return builder.build();
+            return client;
         }
 
         private static OkHttpClient OK_HTTP_CLIENT_INTERCEPTOR(Interceptor interceptor) {
-            if (interceptor != null) {
-                return new OkHttpClient.Builder()
-                        .addInterceptor(interceptor)
+            if (interceptor != null) {//第一次进来 记录下 拦截器的类名
+                if (interceptor.getClass().getSimpleName().equals(simpleName)) {
+                    //如果是一样的 那就直接返回原来的
+                    if (clientInterceptor == null) {//如果为空 那么重新创建一个
+                        return clientInterceptor = new OkHttpClient.Builder()
+                                .addInterceptor(interceptor)
+                                .connectTimeout(TIME_OUT, TimeUnit.SECONDS)
+                                .build();
+                    }
+                    Logger.print("use old interceptor");
+                    return clientInterceptor;
+                } else {
+                    Logger.print("use new interceptor");
+                    simpleName = interceptor.getClass().getSimpleName();
+                    return clientInterceptor = new OkHttpClient.Builder()
+                            .addInterceptor(interceptor)
+                            .connectTimeout(TIME_OUT, TimeUnit.SECONDS)
+                            .build();
+                }
+            } else {//如果拦截器为空 那么直接创建一个新的客户端
+                simpleName = "";
+                return clientInterceptor = new OkHttpClient.Builder()
                         .connectTimeout(TIME_OUT, TimeUnit.SECONDS)
                         .build();
             }
-            return builder.build();
         }
+
     }
 
     //提供一个接口让调用者得到retrofit对象
-    private static  class RestServiceHolder {
+    private static class RestServiceHolder {
 
-        private static  RestService REST_SERVICE = RetrofitHolder.RETROFIT_CLIENT.create(RestService.class);
+        private static RestService REST_SERVICE = RetrofitHolder.RETROFIT_CLIENT.create(RestService.class);
 
         private static RestService getRestServiceInterceptor(Interceptor interceptor) {
             return RetrofitHolder.RETROFIT_CUSTOM_INTERCEPTOR(interceptor).create(RestService.class);
